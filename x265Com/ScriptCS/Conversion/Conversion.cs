24fe2ae7c23ea77c3b75070e2978e5373f6f8bb2
Oxygenexx <oxygenexx3@gmail.com>
2017-04-26 22:23:02 +0200

    Ajouts des fichiers sources manquants (probleme de dl depuis Drive)

---
+++ x265Com/ScriptCS/Conversion/Conversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace x265Com.ScriptCS
+{
+    public class ConversionInfo
+    {
+        public bool isWpp { get; set; }
+        public enum ConversionSpeed { Slow, Regular, Fast }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int EndTime { get; set; }
+        public string InFormat { get; set; }
+        public string OutFormat { get; set; }
+
+    }
+    public class ConversionHelper
+    {
+        
+    }
+}
