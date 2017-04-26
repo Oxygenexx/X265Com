ec2dc97b6b2b78315223e6a1bc90a8ffad6ac27e
Oxygenexx <oxygenexx3@gmail.com>
2017-04-26 22:08:29 +0200

    Ajoutez des fichiers projet.

---
+++ x265Com/ScriptCS/WatchFolder/WatchFolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Configuration;
+
+namespace x265Com.ScriptCS
+{
+    public class WatchFolder
+    {
+        
+        public static void IniWatchFolder()
+        {
+            string WatchFolderPath = ConfigurationManager.AppSettings["WatchFolderPath"].ToString();
+            FileSystemWatcher _FileSystemWatcher = new FileSystemWatcher(WatchFolderPath);
+            var adddress = @"data\";
+            string str = "";
+            DirectoryInfo d = new DirectoryInfo(adddress);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                str = str + ", " + file.Name;
+            }
+        }
+    }
+}
