b5286fcebd088a6b198e0845904702e5656c3281
Shadowmoise <lucas.rittie@gmail.com>
2017-06-02 17:15:46 +0200

    Ajout de la barre progress, affichage de l'avancement, et possibilité de la faire avancer dans le watchfolder,
    ajout de quelques colonnes au tableau Watchfolder aussi,
    
    j'ai viré aussi dans le layout "se connecter" , "s'inscrire"
    
    Tu feras gaffe au Web-config ya moyen que j'ai commit mes chemins de watchfolders sans faire exprès. Désolé ^^

---
+++ x265Com/Scripts/AvancementBaliseProgress.js
@@ -0,0 +1,15 @@
+﻿function modif(val) {
+    var ava = document.getElementById("avancement");
+    if ((ava.value + val) <= ava.max && (ava.value + val) > 0) {
+        ava.value += val;
+    }
+    avancement();
+}
+
+function avancement() {
+    var ava = document.getElementById("avancement");
+    var prc = document.getElementById("pourcentage");
+    prc.innerHTML = ava.value + "%";
+}
+
+avancement(); //Initialisation
